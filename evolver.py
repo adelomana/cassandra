b5ef11369f2b10b037a3421074b97fa4994b3949
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2015-10-09 08:33:49 -0700

    evolver edits

--- evolver.py
+++ evolver.py
@@ -1,1 +1,3 @@
 condider pp to make it parallel
+http://scottsievert.github.io/blog/2014/07/30/simple-python-parallelism/
+http://scottsievert.github.io/blog/2014/05/14/Scientific-Python-tips-and-tricks/
