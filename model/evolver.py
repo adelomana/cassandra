d045cedb6d648aad232e88b2317d9fac857979ca
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2016-05-12 13:36:17 -0700

    update on README

--- model/evolver.py
+++
@@ -1,3 +0,0 @@
-condider pp to make it parallel
-http://scottsievert.github.io/blog/2014/07/30/simple-python-parallelism/
-http://scottsievert.github.io/blog/2014/05/14/Scientific-Python-tips-and-tricks/
